2443f52d9b330795d8339e2e39c54516910360b5
Dimitar Gueorguiev <dimitargueorguiev@Dimitars-MacBook-Pro.local>
2019-06-18 14:45:10 -0400

    using DFT matrix

--- tests/forecaster_test.py
+++ tests/forecaster_test.py
@@ -7,20 +7,14 @@ from ortools.linear_solver import pywraplp
 N=100
 period=10
 S=N/period
-parLambda=1
+parLambda=5
+print('lambda=', parLambda)
 
 dataset = [0.0 for j in range(N)]
-dataset[0]=1.+0.j
-dataset[N/100] = 10.+10.j
-dataset[7*N/100] = 5.+1.j
-dataset[15*N/100] = 3.-2.j
-dataset[23*N/100] = 9.-1.j
-dataset[31*N/100] = 12.-.5j
-dataset[N-N/100] = 10.-10.j
-dataset[N-7*N/100] = 5.-1.j
-dataset[N-15*N/100] = 3.+2.j
-dataset[N-23*N/100] = 9.+1.j
-dataset[N-31*N/100] = 12.+.5j
+#dataset[0]=3.+0.j
+dataset[25*N/100] = 9.+0.j
+#dataset[50]=3.+0.j
+dataset[N-25*N/100] = 9.+0.j
 idft = np.conj(dft(N))
 #time_signal = N*np.fft.ifft(dataset)
 time_signal = idft.dot(np.array(dataset))
@@ -55,7 +49,7 @@ for i in range(0, N):
 for i in range(0, N):
     for j in range(0, N):
         constrEq[i].SetCoefficient(varX[j],np.sin( ( 2.0 * np.pi * i * j ) / N ) )
-        constrEq[i].SetCoefficient(varY[j],np.cos( ( 2.0 * np.pi * i * j ) / N ) )
+        constrEq[i].SetCoefficient(varY[j],-np.cos( ( 2.0 * np.pi * i * j ) / N ) )
 
 constrS1_1 = []
 for i in range(0, S):
@@ -112,5 +106,11 @@ if status != pywraplp.Solver.OPTIMAL:
     exit(1)
 
 print('Solution:')
+y = []
 for i in range(0, N):
+    y.append(complex(varX[i].solution_value(), varY[i].solution_value()))
     print('y_{re}[', i, ']=', varX[i].solution_value(), 'y_{im}[', i, ']=', varY[i].solution_value())
+#idft = np.conj(dft(N))
+recovered_time_signal = idft.dot(np.array(y))
+for i in range(0, N):
+    print('x_{re}[', i, ']=', np.real(recovered_time_signal[i]), 'x_{im}[', i, ']=', np.imag(recovered_time_signal[i]))
